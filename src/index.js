cb1778dc82480813895dcf0922c318fa86bed814
Михаил Пахомов <mihail93@mail.ru>
2023-07-31 21:46:52 +0300

    feat: add function for reverse numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let toString = n.toString();
+    let reverseNumber;
+    if(toString[0] === '-'){
+        toString = toString.slice(1);
+    }
+     reverseNumber = toString.split("").reverse().join("");
+  return +reverseNumber;
 }
+
